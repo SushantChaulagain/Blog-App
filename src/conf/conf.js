a0c0232b39283fc8f0dadb76ed591257bb751b69
SushantChaulagain <sushantchaulagain@gmail.com>
2024-12-06 18:41:17 +0545

    Worked on env and conf

---
+++ src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteUrl: string(process.env.REACT_APP_APPWRITE_URL),
+    appwriteProjectId: string(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: string(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: string(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: string(process.env.REACT_APP_APPWRITE_BUCKET_ID),
+}
+
+export default conf
